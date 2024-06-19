57b0d6a87b5c40e878ba2a1224c8acc955c1c470
tokyospliff <filipalex07@outlook.com>
2024-06-19 23:46:04 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+    'config.lua',
+    'safezone_client.lua'
+}
+
+Author '@tokyospliff'
+
+server_scripts {
+    'config.lua',
+    'safezone_server.lua'
+}
