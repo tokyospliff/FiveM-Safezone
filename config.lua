135bc3b4617e8243d5be982bd60dac70fa6264dd
tokyospliff <filipalex07@outlook.com>
2024-06-19 23:45:55 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.SafeZones = {
+    {x = 215.0, y = -810.0, z = 30.0, radius = 50.0},  -- ex
+    {x = 895.0, y = -179.0, z = 74.0, radius = 30.0},  -- ex
+    -- add more if u want
+}
